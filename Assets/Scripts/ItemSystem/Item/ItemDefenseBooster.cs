87089163ddf134bb986585e919119540fb980f6a
EnochChen1 <enoch.chen.personal@gmail.com>
2023-05-05 22:41:15 -0700

    Merging ItemPromptUI to main

--- Assets/Scripts/ItemSystem/Item/ItemDefenseBooster.cs
+++ Assets/Scripts/ItemSystem/Item/ItemDefenseBooster.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemDefenseBooster : ItemParent{
+    public TextSystemUI textSystem;
+
     public override void Awake(){
         base.Awake();
         category = ItemCategories.Consumable;
@@ -12,5 +14,25 @@ public class ItemDefenseBooster : ItemParent{
 
     public override void Use(Entity entity){
         //Boosts player's defense for a duration
+    }
+
+    public override void InteractWithTarget(Entity entity)
+    {
+        base.InteractWithTarget(entity);
+        if (Input.GetKey("e"))
+        {
+            textSystem.enableText();
+
+            textSystem.setTextIn(name + " added to inventory.");
+
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        textSystem.disableText();
+        Destroy(gameObject);
     }
 }
