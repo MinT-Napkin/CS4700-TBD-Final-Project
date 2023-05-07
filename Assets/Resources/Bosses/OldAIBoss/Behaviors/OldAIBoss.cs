ef0a901dc4c1afb7bc5e84db090304b32d093afd
damiankryzia71 <damiankryzia71@gmail.com>
2023-05-07 16:22:11 -0700

    OldAIBoss follow player behavior

--- Assets/Resources/Bosses/OldAIBoss/Behaviors/OldAIBoss.cs
+++ Assets/Resources/Bosses/OldAIBoss/Behaviors/OldAIBoss.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class OldAIBoss : Boss
 {
+    public override void Awake()
+    {
+        base.Awake();
+        entityStats.walkSpeed = 2f;
+    }
+    public override void RotateEvent()
+    {
+        base.RotateEvent();
+        attackPoint.localPosition = new Vector2(-0.0900000036f,-0.0430000015f);
+    }
 
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, initiateRange);
+
+        Gizmos.color = new Color(141/255f, 13/255f, 225/255f);
+        Gizmos.DrawWireSphere(attackPoint.position, meleeAttackRange);
+    }
 }
