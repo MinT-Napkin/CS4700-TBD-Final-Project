788505d56226c43a0fce2139ae170592404adb85
Feniren <sanders.aidan.c.ss@gmail.com>
2023-05-08 20:21:35 -0700

    Police Robot's Spear has a Physical/Lightning damage split

--- Assets/Scripts/WeaponSystem/Melee/SpearOfThePoliceRobot.cs
+++ Assets/Scripts/WeaponSystem/Melee/SpearOfThePoliceRobot.cs
@@ -7,9 +7,29 @@ public class SpearOfThePoliceRobot : MeleeWeapon{
         base.Awake();
         attackDamage = 15.0f;
         attackRange = 1.5f;
-        damageType = new DamageTypeLightning();
+        damageType = new DamageTypePhysical();
         description = "Stop Resisting";
         name = "Police Robot's Spear";
+    }
+
+    protected override void Attack(){
+        SoundManager.instance.PlaySound(SoundManager.instance.meleeSound);
+        DamageEvent damageEvent;
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach (Collider2D enemy in hitEnemies) {
+            damageEvent = new DamageEvent(0.5f, damageType, attackPoint.parent.gameObject.GetComponent<PlayerClass>(), enemy.gameObject.GetComponent<Enemy>(), DamageCategory.Normal);
+
+            enemy.gameObject.GetComponent<Enemy>().TakeDamage(damageEvent);
+
+            damageType = new DamageTypeLightning();
+
+            damageEvent = new DamageEvent(0.5f, damageType, attackPoint.parent.gameObject.GetComponent<PlayerClass>(), enemy.gameObject.GetComponent<Enemy>(), DamageCategory.Normal);
+
+            enemy.gameObject.GetComponent<Enemy>().TakeDamage(damageEvent);
+        }
+
+        StartCoroutine(AttackCooldown());
     }
 
     public override void Equip(){
