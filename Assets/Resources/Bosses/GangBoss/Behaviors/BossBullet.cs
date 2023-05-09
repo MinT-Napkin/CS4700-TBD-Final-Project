673ec06c3af5ce52871adafbd1de3ab207ecd18b
Minh Tran <mtran8011@yahoo.com>
2023-05-09 10:07:30 -0700

    fixed merge conflicts + removed sample scene (#17)

---
+++ Assets/Resources/Bosses/GangBoss/Behaviors/BossBullet.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBullet : MonoBehaviour
+{
+    public Boss boss;
+    public Vector2 direction;
+    public float bulletSpeed;
+    public float bulletRange;
+    float distanceTraveled;
+    Rigidbody2D rb2d;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        distanceTraveled = 0f;
+        direction = boss.target.position - transform.position;
+        direction.Normalize();
+    }
+
+    void Start()
+    {
+        rb2d.velocity = direction * bulletSpeed;
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, boss.direction);
+    }
+
+    void Update()
+    {
+
+        distanceTraveled += (bulletSpeed * Time.deltaTime);
+        if (distanceTraveled > bulletRange + 10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
